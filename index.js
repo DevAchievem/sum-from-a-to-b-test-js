ba61365b49a0554ef47fcd78e1629303e784b3fb
Mathius Johnson <devachievem@gmail.com>
2020-07-24 16:01:21 -0700

    recursive

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 
-function sum(fromN, toN) {
+const sum = (fromN, toN) => {
   // Sum all the values from fromN up to toN
-}
+  if (fromN === toN) {
+    return fromN;
+  } else {
+    return fromN + sum(fromN + 1, toN);
+  }
 
+};
+// sum(3, 7)  it would sum 3 + 4 + 5 + 6 + 7 and output 25.
 module.exports = sum;
